7fa26acd5d0c4f9f96f8c0684f317850f3833813
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-30 18:10:05 +0530

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import search from "../assets/loupe.png";
+import "../styles/Header.css";
 
 const Header = () => {
     return(
         <div className="nav">
             <div className="nav__menu">
                 <div className="menu__logo">
-
+                    <img src={logo} alt="pokemon logo"/>
                 </div>
                 <div className="menu__search">
-                    
+                    <div className="search__box">
+                        <input type="text" placeholder="Search..."></input>
+                        <img src={search} alt="search"/>
+                    </div>
                 </div>
             </div>
         </div>
