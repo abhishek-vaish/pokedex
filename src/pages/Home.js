d89968f69dd031b1df35bd5a6eaab69dd00bd001
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-06-30 17:40:06 +0530

    add Header Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "../components/Header";
 import "../styles/Home.css";
 
 const Home = () => {
     return(
-        <div>
-            <h1>Home Page</h1>
+        <div className="home">
+            <Header/>
         </div>
     )
 }
