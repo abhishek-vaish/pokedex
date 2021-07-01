02a5ffb4c629ccd31af43aac08f4bb21838b46a8
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-07-01 15:06:37 +0530

    update index.html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
-import "../styles/Home.css";
 import BodyCard from "./BodyCard";
 
 const Home = () => {
-    return(
-        <div className="home">
-            <Header/>
-            <BodyCard/>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Header />
+      <BodyCard />
+    </div>
+  );
+};
 
 export default Home;
